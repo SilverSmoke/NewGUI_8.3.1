99b9d297872f54b712d1a255613307d18b955698
andrei_belikov <omgumail@yandex.ru>
2019-04-05 14:43:47 +0600

    add gitignore

---
+++ src/frames/UserForm.java
@@ -0,0 +1,18 @@
+package frames;
+
+import javax.swing.*;
+import java.text.SimpleDateFormat;
+
+public class UserForm {
+    private JPanel rootPanel;
+    private JTextField surName;
+    private JTextField patronymic;
+    private JTextField name;
+    private JTextField cityOfResidence;
+    private JFormattedTextField birthday;
+    private JButton save;
+
+    public JPanel getRootPanel() {
+        return rootPanel;
+    }
+}
